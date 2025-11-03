6f58a35ad6e3b411703b80f99fd3d7d1264fbf69
navavensosa <nava.vensosa@gmail.com>
2025-11-03 00:43:25 -0500

    first push -- submitting assignment 1

--- assignment_1/server.js
+++ assignment_1/server.js
@@ -1,12 +1,26 @@
-// server.js -- Launch Node.js Server
+// server.js - Launch Node.js Server
 
 let http = require("http");
 let fs = require("fs");
 let server = http.createServer(requestHandler);
 
-server.listen();
+server.listen(8080);
 
-function requestHandler(req, res){
-    console.log(req);
+function requestHandler(req,res){
+    let path = req.url;
+    let filePath = __dirname + path;
+    console.log("Client requesting a file at: ", filePath);
+
+    fs.readFile(filePath, function(err, data) {
+        // check for errors before sending data
+        if (err){
+            console.log("Error getting data! Sending error status");
+            res.writeHead(500);
+            res.end("Server error accessing file!");
+            return;
+        }
+        console.log("Got data! Let's send it to the client.");
+        res.writeHead(200);
+        res.end(data);
+    })
 }
-
